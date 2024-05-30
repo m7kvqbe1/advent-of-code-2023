863352670a97efb641d2ed71db292bd25b5e49b1
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2024-05-30 09:37:57 +0100

    Day 01

---
+++ Day01/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening input file:", err)
+		return
+	}
+	defer file.Close()
+
+	total := 0
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		firstDigit, lastDigit := -1, -1
+
+		// First
+		for _, char := range line {
+			if unicode.IsDigit(char) {
+				firstDigit = int(char - '0')
+				break
+			}
+		}
+
+		// Last - iterate in reverse
+		for i := len(line) - 1; i >= 0; i-- {
+			if unicode.IsDigit(rune(line[i])) {
+				lastDigit = int(line[i] - '0')
+				break
+			}
+		}
+
+		if firstDigit != -1 && lastDigit != -1 {
+			calibrationValue := firstDigit*10 + lastDigit
+			total += calibrationValue
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading input file:", err)
+		return
+	}
+
+	fmt.Println(total)
+}

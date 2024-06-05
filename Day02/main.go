ce607912aac672e928a5c74d83247ad6beb47a42
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2024-06-05 17:53:28 +0100

    Day 02

---
+++ Day02/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening input file:", err)
+		return
+	}
+	defer file.Close()
+
+	// ...
+}
